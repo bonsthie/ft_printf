404855d17826e3495df7967b8126043d1417562a
Barnabe Bonnet <babonnet@3B4.42angouleme.fr>
2023-11-09 20:06:24 +0100

    yo

--- ft_printf.c
+++ ft_printf.c
@@ -1,14 +1,8 @@
 #include "ft_printf.h"
 #include <stdio.h>
 
-static void ft_putnbr_base(long long n, char *base, int base_size, int *count)
+static void ft_putnbr_base(unsigned long n, char *base, unsigned long base_size, int *count)
 {
-	if (n < 0)
-	{
-		ft_putchar_fd('-', 1);
-        (*count)++;
-		n = -n;
-	}
 	if (n >= base_size)
 	{
 		ft_putnbr_base(n / base_size, base, base_size, count);
@@ -19,6 +13,30 @@ static void ft_putnbr_base(long long n, char *base, int base_size, int *count)
         ft_putchar_fd(base[n], 1);
         (*count)++;
     }
+}
+
+void	ft_putnbr_count(int n, int *count)
+{
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", 1);
+		(*count) += 11; 
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', 1);
+		(*count)++;
+		n = -n;
+	}
+	if (n < 10)
+	{
+		ft_putchar_fd(n + '0', 1);
+		(*count)++;
+		return ;
+	}
+	ft_putnbr_count(n / 10, count);
+	ft_putnbr_count(n % 10, count);
 }
 
 static int manage_char(va_list var, const char type)
@@ -46,7 +64,7 @@ static int print_variable(va_list var, const char type)
 	if (type == 'c' || type == 's')
 		count = manage_char(var, type);
 	else if (type == 'd' || type == 'i')
-		ft_putnbr_base(va_arg(var, long long), "0123456789", 10, &count);
+		ft_putnbr_count(va_arg(var, long long), &count);
 	else if (type == 'u')
 		ft_putnbr_base(va_arg(var, unsigned long long), "0123456789", 10, &count);
 	else if (type == 'x')
@@ -57,7 +75,7 @@ static int print_variable(va_list var, const char type)
 	{
 		ft_putstr_fd("0x", 1);
         count += 2;
-		ft_putnbr_base((unsigned long long) va_arg(var, void *), "0123456789abcdef", 16, &count);
+		ft_putnbr_base((unsigned long) va_arg(var, void *), "0123456789abcdef", 16, &count);
 	}
 	else if (type == '%')
 	{
